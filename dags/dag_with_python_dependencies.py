8f69f2e1656a6f236bbbbbae0594e67421c31a63
GDIATTA <147615966+GDIATTA@users.noreply.github.com>
2024-01-24 04:13:14 +0100

    Update dag_with_python_dependencies.py

--- dags/dag_with_python_dependencies.py
+++ dags/dag_with_python_dependencies.py
@@ -5,7 +5,7 @@ from airflow.operators.python import PythonOperator
 
 
 default_args = {
-    'owner': 'coder2j',
+    'owner': 'gauss',
     'retry': 5,
     'retry_delay': timedelta(minutes=5)
 }
@@ -24,7 +24,7 @@ def get_matplotlib():
 with DAG(
     default_args=default_args,
     dag_id="dag_with_python_dependencies_v03",
-    start_date=datetime(2021, 10, 12),
+    start_date=datetime(2024, 1, 24),
     schedule_interval='@daily'
 ) as dag:
     task1 = PythonOperator(
