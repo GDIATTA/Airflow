002d5d49b06f438eee0a45b982187a44854c05b6
GDIATTA <gaussjatta1@outlook.fr>
2024-02-07 09:14:20 +0100

    update

--- dags/dag_with_air_poll.py
+++ dags/dag_with_air_poll.py
@@ -2,6 +2,8 @@ from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.providers.http.sensors.http import HttpSensor
+import json
+from airflow.providers.http.operators.http import SimpleHttpOperator
 
 
 default_args = {
@@ -12,15 +14,23 @@ default_args = {
 
 
 with DAG(
-    dag_id='dag_with_postgres_operator_v03',
+    dag_id='dag_with_air_poll',
     default_args=default_args,
-    start_date=datetime(2024, 2, 7),
-    schedule_interval='@daily'
-    catchup=False
+    start_date=datetime(2024, 2, 6),
+    schedule_interval='@daily',
+    #catchup=False
 ) as dag:
     task1 = HttpSensor(
-        task_id ='is_weather_api_ready',
+        task_id ='conect_api_air_poll',
         http_conn_id='weathermap_api',
-        endpoint='/data/2.5/weather?q=Portland&APPID=5031cde3d1a8b9469fd47e998d7aef79'
+        endpoint='/feed/toulouse/?token=47745c182fb9aee4c14e5234e4ad010b13083722'
         )
-    task1
+    task2_extract = SimpleHttpOperator(
+        task_id = 'extract_air_poll_data',
+        http_conn_id = 'weathermap_api',
+        endpoint='/feed/toulouse/?token=47745c182fb9aee4c14e5234e4ad010b13083722',
+        method = 'GET',
+        response_filter= lambda r: json.loads(r.text),
+        log_response=True
+        )
+    task1 >> task2_extract
