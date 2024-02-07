18aac0a962ffcbd14b6772dc0ac23f3762f967e6
GDIATTA <gaussjatta1@outlook.fr>
2024-02-07 05:08:02 +0100

    added python

--- dags/dag_with_air_poll.py
+++ dags/dag_with_air_poll.py
@@ -15,7 +15,7 @@ with DAG(
     dag_id='dag_with_postgres_operator_v03',
     default_args=default_args,
     start_date=datetime(2024, 2, 7),
-    schedule_interval=@daily
+    schedule_interval='@daily'
     catchup=False
 ) as dag:
     task1 = HttpSensor(
@@ -23,3 +23,4 @@ with DAG(
         http_conn_id='weathermap_api',
         endpoint='/data/2.5/weather?q=Portland&APPID=5031cde3d1a8b9469fd47e998d7aef79'
         )
+    task1
